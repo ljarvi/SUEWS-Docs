82f7fd0562cb663de8894e5b7b4b981a656246e1
Ting Sun <sunting.05@gmail.com>
2018-03-20 20:34:21 +0000

    fix the table wrapping/scrolling issue.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ html_theme_path = ["_themes"]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #
-# html_static_path = ['_static']
+html_static_path = ['_static']
 # html_context = {
 #     'css_files': [
 #         '_static/theme_overrides.css',  # override wide tables in RTD theme
@@ -187,3 +187,9 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+
+# Fix for scrolling tables in the RTD-theme
+# https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
+def setup(app):
+    app.add_stylesheet('theme_overrides.css')
